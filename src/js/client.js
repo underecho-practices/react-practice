997862aacbe311c418a15490b2cdf47c4104f4bc
under <poke2000@live.jp>
2020-09-02 23:48:50 +0900

    components Tutorial

--- src/js/client.js
+++ src/js/client.js
@@ -1,17 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-class Layout extends React.Component {
-  render() {
-    name = "under.";
-    return (
-      <div>
-          <h1>It's {name}</h1>
-          <h2>It's {((num) => {return 1 + num;})(3)}</h2>
-      </div>
-    );
-  }
-}
+import Layout from "./components/Layout";
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
