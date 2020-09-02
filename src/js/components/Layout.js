336db8f8f8358702f61dc1fe696488ba954b0c5c
under <poke2000@live.jp>
2020-09-02 23:54:34 +0900

    Components Tutorial

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Header from "./Header"
+import Footer from "./Footer"
+
 
 export default class Layout extends React.Component {
   constructor() {
@@ -7,7 +10,11 @@ export default class Layout extends React.Component {
   }
   render() {
     return (
-      <h1>It's {this.name}!</h1>
+      <div>
+        <Header />
+        <h1>It's {this.name}!</h1>
+        <Footer />
+      </div>
     );
   }
 }
